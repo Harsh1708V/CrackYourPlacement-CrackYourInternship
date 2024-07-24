2b948e3c9fb073d54b6f6ec19a35f80d5373ae07
Harsh Waibhav <148435290+Harsh1708V@users.noreply.github.com>
2024-07-25 00:56:15 +0530

    Time: 9 ms (58.37%), Space: 16.9 MB (21.51%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,24 +1,31 @@
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size(); 
         int m = matrix[0].size();
-        int col[m] = {0};
-        int row[n] = {0};
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m;j++) {
-                if(matrix[i][j] == 0) {
-                    row[i] ,col[j] == 1 ;
+        vector<int> col(m, 0);
+        vector<int> row(n, 0);
+
+        // First pass to find zeroes and mark corresponding rows and columns
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
                 }
             }
         }
-        for(int i = 0; i<n; i++){
-            for(int j = 0 ; j<m ; j++) {
-                if(row[i] || col [j]) {
+
+        // Second pass to set the corresponding rows and columns to zero
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (row[i] == 1 || col[j] == 1) {
                     matrix[i][j] = 0;
                 }
             }
         }
-        return matrix;
     }
 };
